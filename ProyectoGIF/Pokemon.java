5056f9f25750736e9ec0734bce9ffca80bb44c45
EnderPeace420 <daniel5tecno@gmail.com>
2023-05-04 15:48:02 +0200

    PRUEBA
    Prueba1

---
+++ ProyectoGIF/Pokemon.java
@@ -0,0 +1,48 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ProyectoGIF;
+
+/**
+ *
+ * @author alumnot
+ */
+public class Pokemon {
+
+   private String name;
+   private Integer weight;
+   private String sprites;
+
+    public Pokemon() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getWeight() {
+        return weight;
+    }
+
+    public void setWeight(Integer weight) {
+        this.weight = weight;
+    }
+
+    public String getSprites() {
+        return sprites;
+    }
+
+    public void setSprites(String sprites) {
+        this.sprites = sprites;
+    }
+
+    
+   
+   
+   
+}
